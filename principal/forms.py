bf5572ea76fbe9cb5af135564e9ecfdfcebebf11
Cristian <cristianvillalobos666@gmail.com>
2024-01-20 10:08:58 -0300

    Added view and template for detailed blog post as well as CSS for the site.

---
+++ principal/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Profile, BlogPost
+from django import forms
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('phone_no', 'bio', 'facebook', 'instagram', 'linkedin', 'image', )
+     
+        
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = BlogPost
+        fields = ('title', 'slug', 'content', 'image','text','description')
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Title of the Blog'}),
+            'slug': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Copy the title with no space and a hyphen in between'}),
+            'content': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Content of the Blog'}),
+            'text': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Text of the Blog'}), # Utilizar CKEditorWidget para el campo 'text'
+            'description': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Description of the Blog'}),
+        }
