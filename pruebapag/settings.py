e83014d94becc3adc45c5b200cce10b9a3b5722e
Cristian <cristianvillalobos666@gmail.com>
2024-01-20 10:28:03 -0300

    HUMANIZE

--- pruebapag/settings.py
+++ pruebapag/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'principal'
+    'principal',
+    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
